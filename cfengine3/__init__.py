e99d740e37fd7ee3225ca32d9da7ba49d35b44ce
Juliano Martinez <juliano@falaserio.org>
2011-07-13 12:02:36 -0300

    fixed setup.py

--- cfengine3/__init__.py
+++ cfengine3/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 VERSION = tuple(map(int, __version__.split('.')))
 __all__ = ['cfengine3']
 __author__ = 'Juliano Martinez <juliano@martinez.io>'
--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='cfengine3',
-      version='0.0.2',
+      version='0.0.3',
       description='A simple way to extend your cfengine3 using python',
       author='Juliano Martinez',
       author_email='juliano@martinez.io',
       url='https://github.com/ncode/cfengine3',
-      packages=['cfengine3'],
+      packages=find_packages(exclude=["tests"]),
      )
